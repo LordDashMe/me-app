4f596fb67cebd4ffac93882ebc9474b279a6bfc4
LordDashMe <reyesjoshuaclifford@gmail.com>
2019-07-02 17:50:24 +0800

    chore: change from feature to functional

--- src/UserManagement/Domain/Repository/UserLoginRepository.php
+++ src/UserManagement/Domain/Repository/UserLoginRepository.php
@@ -7,6 +7,4 @@ use UserManagement\Domain\Entity\UserLogin;
 interface UserLoginRepository
 {
     public function get(UserLogin $userLogin);
-    
-    public function isApproved(UserLogin $userLogin): bool;
 }
